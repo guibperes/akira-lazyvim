cbb1b7e648ad8b58f7d0a905fcb5d6282437a7a0
Guilherme Beidaki Peres <guibperes@proton.me>
2025-09-30 10:45:30 -0300

    backup: lazyvim configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = false
+opt.linebreak = true
+opt.ruler = true
+opt.wrap = true
+opt.spelllang = { "en", "pt_br" }
