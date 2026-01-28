b1a93a06a5f33fcb9dc9cdc01461adb661d265e7
Guilherme Beidaki Peres <guibperes@proton.me>
2026-01-28 11:45:43 -0300

    LaTeX config

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,10 +1,13 @@
 return {
   {
     "lervag/vimtex",
+    lazy = false,
     config = function()
       vim.g.vimtex_compiler_latexmk = {
         out_dir = "build",
       }
+      vim.g.vimtex_compiler_latexmk.callback = 1
+      vim.g.vimtex_view_method = 'zathura'
     end,
   },
 }
