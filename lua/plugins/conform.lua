200ac15191258644e592dfcbcbe79f2280d50ce3
Guilherme Beidaki Peres <guibperes@proton.me>
2026-08-10 11:31:41 -0300

    Added tex-fmt for latex files

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        tex = { "tex-fmt" },
+        bib = { "tex-fmt" },
+        plaintex = { "tex-fmt" },
+      },
+    },
+  },
+}
